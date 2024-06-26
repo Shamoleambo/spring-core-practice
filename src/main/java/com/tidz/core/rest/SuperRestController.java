cb96700cf42ba5663e96d1d0b63e8770e621d75a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-06-26 08:31:33 -0300

    create rest controller

---
+++ src/main/java/com/tidz/core/rest/SuperRestController.java
@@ -0,0 +1,25 @@
+package com.tidz.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tidz.core.entities.Stupid;
+
+@RestController
+@RequestMapping("/api")
+public class SuperRestController {
+
+	private Stupid stupid;
+
+	@Autowired
+	public SuperRestController(Stupid stupid) {
+		this.stupid = stupid;
+	}
+
+	@GetMapping("/stupidSays")
+	public String checksForTheStupid() {
+		return this.stupid.speak();
+	}
+}
